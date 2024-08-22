f289948d5162a67a0cfbd47f7190bfd056490add
Aleksei Pepeliaev <a.pepelyaev@superkassa.net>
2024-08-22 11:06:19 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [height, setHeight] = useState(30)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div 
+      style={{height}}
+        className='changeHeightEl'
+        onClick={() => setHeight(150*Math.random() + 30)} >Random height element</div>
+      <div className="card">
+        <button onClick={() => {
+          setCount((count) => count + 1)
+          setHeight(150*Math.random() + 30)
+          }}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
